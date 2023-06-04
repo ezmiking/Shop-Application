e10976ea3a4269e1a135dd03ae3cdd0c6e0ca4d0
ezmiking <alipir82.alipir82@gmail.com>
2023-06-04 15:59:01 +0330

    LoginMenu

---
+++ Shop Application/src/main/java/view/login/LoginMenu.java
@@ -0,0 +1,14 @@
+package view.login;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LoginMenu extends Application {
+    public static void main(String[] args) {
+
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
