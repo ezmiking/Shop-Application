93716f9435d77caed231b5899f879ffba88480b7
ezmiking <alipir82.alipir82@gmail.com>
2023-07-02 14:05:04 +0330

    showPassword prof

--- Shop Application/src/main/java/view/profile/ProfileMenuController.java
+++ Shop Application/src/main/java/view/profile/ProfileMenuController.java
@@ -1,4 +1,28 @@
 package view.profile;
 
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class ProfileMenuController {
+    public TextField name;
+    public TextField lastName;
+    public TextField phoneNumber;
+    public TextField gmail;
+    public TextField textFieldPassword;
+    public PasswordField passwordField;
+    public RadioButton showPassword;
+
+    public void showPassword(MouseEvent mouseEvent) {
+        if (showPassword.isSelected()) {
+            textFieldPassword.setText(passwordField.getText());
+            passwordField.setVisible(false);
+            textFieldPassword.setVisible(true);
+        }
+        else {
+            passwordField.setVisible(true);
+            textFieldPassword.setVisible(false);
+        }
+    }
 }
